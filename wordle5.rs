c753bf750485a1a0978acde7ea745011e1557406
Bart Massey <bart.massey@gmail.com>
2022-09-05 02:41:26 -0700

    moved to word grouping by letter

--- wordle5.rs
+++ wordle5.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-use ordered_float::NotNan;
-
 fn five_letter(word: &str) -> Option<(u32, String)> {
     if word.chars().any(|c| !c.is_ascii_lowercase()) {
         panic!("weird word {word} in dict");
@@ -46,37 +44,26 @@ fn letter_freqs(ids: &[u32]) -> [usize; 26] {
     counts
 }
 
-fn score_id(freqs: &[usize; 26], id: u32) -> NotNan<f32> {
-    let mut sum = 0.0;
-    for (c, &f) in freqs.iter().enumerate() {
-        if id & (1 << c) != 0 {
-            sum += f32::log2(f as f32);
-        }
-    }
-    NotNan::new(sum).unwrap()
-}
-
-#[test]
-fn test_score_id() {
-    let mut freqs = [0; 26];
-    for i in 0..5 {
-        freqs[i] = 2;
-    }
-    let id = five_letter("abcde").unwrap().0;
-    assert_eq!(id, 0b11111);
-    assert_eq!(score_id(&freqs, id).into_inner(), 5.0);
-}
-
 fn main() {
     let dict = assemble_dicts();
-    let mut ids: Vec<u32> = dict.keys().copied().collect();
+    let ids: Vec<u32> = dict.keys().copied().collect();
     let freqs = letter_freqs(&ids);
-    let scores: HashMap<u32, NotNan<f32>> = ids
-        .iter()
-        .map(|&id| (id, score_id(&freqs, id)))
-        .collect();
-    ids.sort_unstable_by_key(|id| scores[id]);
-    for id in ids {
-        println!("{}: {}", dict[&id], scores[&id]);
+
+    let mut letter_ids = Vec::new();
+    for (c, f) in freqs.iter().enumerate() {
+        let cs: Vec<u32> = ids
+            .iter()
+            .filter(|&&id| id & (1 << c) != 0)
+            .copied()
+            .collect();
+        letter_ids.push((f, cs));
+    }
+
+    for (c, (f, ids)) in letter_ids.iter().enumerate() {
+        let ch = char::from_u32(c as u32 + 'a' as u32).unwrap();
+        println!("{} ({})", ch, f);
+        for id in ids {
+            println!("    {}", dict[id]);
+        }
     }
 }
